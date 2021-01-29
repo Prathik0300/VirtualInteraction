a327ec102da64812bafb52ff1486a78b25ea751d
prathik pugazhenthi <prathik0300@gmail.com>
2021-01-29 19:25:39 +0530

    Done

--- Virtual_interaction.py
+++ Virtual_interaction.py
@@ -2,24 +2,34 @@ import cv2 as cv
 import numpy as np
 from caliberation import caliberation
 import pyautogui as gui
+from move import find
 
+def movePointer(x0,y0):
+    gui.moveTo(x0,y0,duration=1)
+    if cv.waitKey(1) & 0xFF==ord('s'):
+        print(x0,y0)
+        gui.click(x0,y0,clicks=1)
+    elif cv.waitKey(1) & 0xFF==ord('d'):
+        print(x0,y0)
+        gui.click(x0,y0,clicks=2)
+    elif cv.waitKey(1) & 0xFF==ord('f'):
+        gui.mouseDown(button='left')
+    elif cv.waitKey(1) & 0xFF==ord('g'):
+        gui.mouseUp(button='left')
 def rescale(frame,scale=1.25):
     width = int(frame.shape[1]*scale)
     height = int(frame.shape[0]*scale)
     dimension = (width,height)
     return cv.resize(frame,dimension,interpolation=cv.INTER_CUBIC)
-
-    
 gui.FAILSAFE = False
 Lower,Upper = caliberation()
 video = cv.VideoCapture(0)
 isTrue=1
 x0,y0=0,0
-
 point_color = (0,0,255)
 select_color = (0,255,0)
-#cv.namedWindow('frame', cv.WINDOW_NORMAL)
-#cv.setWindowProperty('frame', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+cv.namedWindow('frame', cv.WINDOW_NORMAL)
+cv.setWindowProperty('frame', cv.WND_PROP_FULLSCREEN, cv.WINDOW_NORMAL)
 count=1
 while isTrue==1:
     isTrue,frame = video.read()
@@ -29,23 +39,20 @@ while isTrue==1:
     frame = cv.flip(frame,1)
     hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
     mask = cv.inRange(hsv,np.array(Lower),np.array(Upper))
-    erode = cv.erode(mask,(5,5),iterations=2)
+    erode = cv.erode(mask,(5,5),iterations=1)
     contour,hierarchy = cv.findContours(erode,cv.RETR_TREE,cv.CHAIN_APPROX_NONE)
     if contour:
         c = max(contour,key=cv.contourArea)
         x,y,w,h = cv.boundingRect(c)
         cv.rectangle(erode,(x,y),(x+w,y+h),(255,255,255),thickness=2)
         #cv.line(erode,((x+w//2),(y+h//2)),(0,0),(255,0,0),4)
-
-        x0,y0=(x+w//2)*5,(y+h//2)*4
-        gui.moveTo(x0,y0,duration=1.5)
+        x0,y0=(x+w//2)*5,(y+h//2)*5
+        movePointer(x0,y0)
         show = cv.bitwise_or(blank,erode)
-        cv.imshow("frame",show)
-        if cv.waitKey(1) & 0xFF==ord('s'):
-            print(x0,y0)
-            gui.doubleClick(x0,y0)
+        cv.imshow("frame",show)     
     else:
         x0,y0=0,0
+    find()
     if cv.waitKey(1) & 0xFF==ord('a'):
         break    
 video.release()
--- move.py
+++ move.py
@@ -1,1 +1,20 @@
-import pyautogui as gui
+import win32gui
+
+def windowEnumerationHandler(hwnd, top_windows):
+    top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+def find():
+    results = []
+    top_windows = []
+    win32gui.EnumWindows(windowEnumerationHandler, top_windows)
+    for i in top_windows:
+        if i[1]=='frame':
+            try:
+                win32gui.ShowWindow(i[0],5)
+                win32gui.SetForegroundWindow(i[0])
+                break
+            except:
+                break
+if __name__ == '__main__':
+    find()
+    
